c27ea05eb17cec7c56a2ba5c252e2e7c0533bf72
Stef <git@srilq.email>
2021-03-10 16:25:18 +0000

    Add docs

---
+++ README.md
@@ -0,0 +1,26 @@
+# outerspace-rs
+
+[![Crates.io version](https://img.shields.io/crates/v/outerspace.svg?style=flat-square)](https://crates.io/crates/outerspace)
+[![Crates.io downloads](https://img.shields.io/crates/d/outerspace.svg?style=flat-square)](https://crates.io/crates/outerspace)
+[![docs.rs docs](https://img.shields.io/badge/docs-latest-blue.svg?style=flat-square)](https://docs.rs/outerspace)
+
+This is the Rust version of the [outerspace JavaScript package](https://www.npmjs.com/package/outerspace).
+
+Methods for prefixing and suffixing the non-whitespace characters in a string.
+
+# Example
+
+```rust
+let prefixed = outerspace::prefix_non_whitespace("\n\nHello hello\n\n", "> ");
+assert_eq!(prefixed, "\n\n> Hello hello\n\n");
+```
+
+```rust
+let suffixed = outerspace::suffix_non_whitespace("\n\nHello hello\n\n", "!");
+assert_eq!(suffixed, "\n\nHello hello!\n\n");
+```
+
+```rust
+let wrapped = outerspace::wrap_non_whitespace("\n\nHello hello\n\n", "**", "**");
+assert_eq!(wrapped, "\n\n**Hello hello**\n\n");
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,26 @@
-fn is_non_whitespace(c: char) -> bool {
-    !c.is_whitespace()
+#![deny(missing_docs)]
+
+//! Methods for prefixing and suffixing the non-whitespace characters in a string.
+//!
+//! # Examples
+//!
+//! ```
+//! let wrapped = outerspace::wrap_non_whitespace("\n\nHello hello\n\n", "**", "**");
+//! assert_eq!(wrapped, "\n\n**Hello hello**\n\n");
+//! ```
+//!
+//! ```
+//! let prefixed = outerspace::prefix_non_whitespace("\n\nHello hello\n\n", "> ");
+//! assert_eq!(prefixed, "\n\n> Hello hello\n\n");
+//! ```
+//!
+//! ```
+//! let suffixed = outerspace::suffix_non_whitespace("\n\nHello hello\n\n", "!");
+//! assert_eq!(suffixed, "\n\nHello hello!\n\n");
+//! ```
+
+fn is_non_whitespace(char: char) -> bool {
+    !char.is_whitespace()
 }
 
 fn format_wrap(
@@ -29,17 +50,47 @@ fn format_wrap(
     }
 }
 
+/// Insert a prefix and a suffix into the string. The prefix is inserted before the first non-whitespace character. The suffix is inserted after the last non-whitespace character.
+///
+/// Returns a heap-allocated String.
+///
+/// # Example
+///
+/// ```
+/// let wrapped = outerspace::wrap_non_whitespace("\n\nHello hello\n\n", "**", "**");
+/// assert_eq!(wrapped, "\n\n**Hello hello**\n\n");
+/// ```
 pub fn wrap_non_whitespace(s: &str, prefix: &str, suffix: &str) -> String {
     let first_non_whitespace = s.find(is_non_whitespace);
     let last_non_whitespace = s.rfind(is_non_whitespace);
     format_wrap(s, prefix, suffix, first_non_whitespace, last_non_whitespace)
 }
 
+/// Insert a prefix into the string before the first non-whitespace character.
+///
+/// Returns a heap-allocated String.
+///
+/// # Example
+///
+/// ```
+/// let prefixed = outerspace::prefix_non_whitespace("\n\nHello hello\n\n", "> ");
+/// assert_eq!(prefixed, "\n\n> Hello hello\n\n");
+/// ```
 pub fn prefix_non_whitespace(s: &str, prefix: &str) -> String {
     let first_non_whitespace = s.find(is_non_whitespace);
     format_wrap(s, prefix, "", first_non_whitespace, None)
 }
 
+/// Insert a suffix into the string after the last non-whitespace character.
+///
+/// Returns a heap-allocated String.
+///
+/// # Example
+///
+/// ```
+/// let suffixed = outerspace::suffix_non_whitespace("\n\nHello hello\n\n", "!");
+/// assert_eq!(suffixed, "\n\nHello hello!\n\n");
+/// ```
 pub fn suffix_non_whitespace(s: &str, suffix: &str) -> String {
     let last_non_whitespace = s.rfind(is_non_whitespace);
     format_wrap(s, "", suffix, None, last_non_whitespace)
